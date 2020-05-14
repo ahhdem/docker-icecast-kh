c88dc9d897fcb13100ace22558f6b7a84eda845e
adam <adam@onethree.net>
2020-05-14 00:44:44 -0700

    Update path and source password in tokenizing

--- scripts/tokenize.sh
+++ scripts/tokenize.sh
@@ -10,45 +10,45 @@ ICECAST_STREAM_NAME=${ICECAST_STREAM_NAME:-'amazing radio'}
 # Tokenize
 sed -i'' \
   -e "s/ICECAST_HOST/${ICECAST_HOST}/g" \
-  -e "s/ICECAST_PASSWORD/${ICECAST_PASSWORD}/g" \
+  -e "s/ICECAST_SOURCE_PASSWORD/${ICECAST_SOURCE_PASSWORD}/g" \
   -e "s/ICECAST_STREAM_NAME/${ICECAST_STREAM_NAME}/g" \
   -e "s^ICECAST_STREAM_URL^${ICECAST_STREAM_URL}^g" \
   -e "s/ICECAST_EZSTREAM_MOUNT/${ICECAST_EZSTREAM_MOUNT}/g" \
-  /etc/ezstream.xml
+  /etc/icecast/ezstream.xml
 
 
 if [ -n "$ICECAST_SOURCE_PASSWORD" ]; then
-    sed -i'' -e "s/<source-password>[^<]*<\/source-password>/<source-password>$ICECAST_SOURCE_PASSWORD<\/source-password>/g" /etc/icecast.xml
+    sed -i'' -e "s/<source-password>[^<]*<\/source-password>/<source-password>$ICECAST_SOURCE_PASSWORD<\/source-password>/g" /etc/icecast/icecast.xml
 fi
 
 if [ -n "$ICECAST_RELAY_PASSWORD" ]; then
-    sed -i'' -e "s/<relay-password>[^<]*<\/relay-password>/<relay-password>$ICECAST_RELAY_PASSWORD<\/relay-password>/g" /etc/icecast.xml
+    sed -i'' -e "s/<relay-password>[^<]*<\/relay-password>/<relay-password>$ICECAST_RELAY_PASSWORD<\/relay-password>/g" /etc/icecast/icecast.xml
 fi
 
 if [ -n "$ICECAST_ADMIN_PASSWORD" ]; then
-    sed -i'' -e "s/<admin-password>[^<]*<\/admin-password>/<admin-password>$ICECAST_ADMIN_PASSWORD<\/admin-password>/g" /etc/icecast.xml
+    sed -i'' -e "s/<admin-password>[^<]*<\/admin-password>/<admin-password>$ICECAST_ADMIN_PASSWORD<\/admin-password>/g" /etc/icecast/icecast.xml
 fi
 
 if [ -n "$ICECAST_PASSWORD" ]; then
-    sed -i'' -e "s/<password>[^<]*<\/password>/<password>$ICECAST_PASSWORD<\/password>/g" /etc/icecast.xml
+    sed -i'' -e "s/<password>[^<]*<\/password>/<password>$ICECAST_PASSWORD<\/password>/g" /etc/icecast/icecast.xml
 fi
 
 if [ -n "$ICECAST_LOCATION" ]; then
-    sed -i'' -e "s/<location>[^<]*<\/location>/<location>$ICECAST_LOCATION<\/location>/g" /etc/icecast.xml
+    sed -i'' -e "s/<location>[^<]*<\/location>/<location>$ICECAST_LOCATION<\/location>/g" /etc/icecast/icecast.xml
 fi
 
 if [ -n "$ICECAST_HOSTNAME" ]; then
-    sed -i'' -e "s/<hostname>[^<]*<\/hostname>/<hostname>$ICECAST_HOSTNAME<\/hostname>/g" /etc/icecast.xml
+    sed -i'' -e "s/<hostname>[^<]*<\/hostname>/<hostname>$ICECAST_HOSTNAME<\/hostname>/g" /etc/icecast/icecast.xml
 fi
 
 if [ -n "$ICECAST_ADMIN" ]; then
-    sed -i'' -e "s/<admin>[^<]*<\/admin>/<admin>$ICECAST_ADMIN<\/admin>/g" /etc/icecast.xml
+    sed -i'' -e "s/<admin>[^<]*<\/admin>/<admin>$ICECAST_ADMIN<\/admin>/g" /etc/icecast/icecast.xml
 fi
 
 if [ -n "$ICECAST_MAX_SOURCES" ]; then
-    sed -i'' -e "s/<sources>[^<]*<\/sources>/<sources>$ICECAST_MAX_SOURCES<\/sources>/g" /etc/icecast.xml
+    sed -i'' -e "s/<sources>[^<]*<\/sources>/<sources>$ICECAST_MAX_SOURCES<\/sources>/g" /etc/icecast/icecast.xml
 fi
 
 if [ -n "$ICECAST_MAX_LISTENERS" ]; then
-    sed -i'' -e "s/<clients>[^<]*<\/clients>/<clients>$ICECAST_MAX_LISTENERS<\/clients>/g" /etc/icecast.xml
+    sed -i'' -e "s/<clients>[^<]*<\/clients>/<clients>$ICECAST_MAX_LISTENERS<\/clients>/g" /etc/icecast/icecast.xml
 fi
